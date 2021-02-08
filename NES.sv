ba180c52b89093285203abc6acdc6bfe5cf21b4d
antivorg <antivorg@yahoo.co.uk>
2021-02-08 22:23:13 +0000

    Created top level

---
+++ NES.sv
@@ -0,0 +1,18 @@
+/*
+*   NES
+*
+*   - Top level of design
+*/
+
+module NES (
+    input logic clock,
+    // io ports
+);
+
+    CPU6502 CPU65020 (.*);
+    SRAM SRAMCPU (.*);
+    SRAM SRAMPPU (.*);
+    PPU PPU0 (.*);
+    dataBus dataBus0 ();
+
+endmodule
