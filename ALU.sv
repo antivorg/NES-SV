c674b33d358658477437290b7d3cab75ba1b7387
antivorg <antivorg@yahoo.co.uk>
2021-02-09 15:23:24 +0000

    Created ALU file and defined some ports

---
+++ ALU.sv
@@ -0,0 +1,17 @@
+/*
+*
+* ALU
+*
+*/
+
+module ALU (
+    input logic [7:0] A, B,
+    input logic statusReg[7:0],
+    input logic decEnable, carrIn,
+    output logic [7:0] resultReg,
+    output logic overFlow, carrOut, halfCarry, 
+);
+
+    
+
+endmodule
